3d49574c9b9ff1cfab85019c393123b545f5c097
@brody4hire - C. Jonathan Brody <brody4hire@brodycj.com>
2025-02-28 14:27:47 -0500

    xxx-comments

--- rustls/src/builder.rs
+++ rustls/src/builder.rs
@@ -163,12 +163,14 @@ use crate::{ClientConfig, ServerConfig};
 #[derive(Clone)]
 pub struct ConfigBuilder<Side: ConfigSide, State> {
     pub(crate) state: State,
+    // XXX XXX
     pub(crate) provider: Rc1<CryptoProvider>,
     pub(crate) time_provider: RcX<dyn TimeProvider>,
     pub(crate) side: PhantomData<Side>,
 }
 
 impl<Side: ConfigSide, State> ConfigBuilder<Side, State> {
+    // XXX XXX RETURN TYPE ALIAS
     /// Return the crypto provider used to construct this builder.
     pub fn crypto_provider(&self) -> &Rc1<CryptoProvider> {
         &self.provider
